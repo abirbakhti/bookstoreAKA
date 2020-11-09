6a6004a9e12b5af5666d46aeadc1c0c7196079db
abirbakhti <41122560+abirbakhti@users.noreply.github.com>
2020-11-09 10:20:16 +0100

    Step 1

---
+++ src/Bookstore.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bookstore {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Donner la quantité: ");
+		int qte=s.nextInt();
+		System.out.println("Donner le prix unitaire: ");
+		double unitPrice=s.nextDouble();
+		Utility u = new Utility();
+		System.out.println("Total: " + u.calculateTotalPrice(qte, unitPrice));
+	}
+
+}
---
+++ src/Utility.java
@@ -0,0 +1,6 @@
+
+public class Utility {
+	public double calculateTotalPrice(int qte, double unitPrice) {
+		return qte*unitPrice;
+	}
+}
