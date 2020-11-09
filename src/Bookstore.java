a7af50c6b27ec0877984a6d1b365044298461ed4
karim-07 <karimbensaad07@gmail.com>
2020-11-09 15:27:23 +0100

    partie 1-5 step 2

--- src/Bookstore.java
+++ src/Bookstore.java
@@ -4,12 +4,18 @@ public class Bookstore {
 
 	public static void main(String[] args) {
 		Scanner s= new Scanner(System.in);
-		System.out.println("Donner la quantité: ");
+		System.out.println("Donner le titre: ");
 		int qte=s.nextInt();
 		System.out.println("Donner le prix unitaire: ");
 		double unitPrice=s.nextDouble();
 		Utility u = new Utility();
 		System.out.println("Total: " + u.calculateTotalPrice(qte, unitPrice));
+		
+		
+		
+		
+		
+		
 	}
 
 }
